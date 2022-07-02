27f35f1fdc05e8967e9ebd78c7298410cb3580cc
greatbrain <erickfania@gmail.com>
2022-07-02 16:03:30 -0400

    First repo commit

---
+++ scripts/scrapper.py
@@ -0,0 +1,44 @@
+import twint as tw
+
+
+class Scrapper:
+      def __init__(self):
+          self.keywords = list()
+
+      
+      def search(self):
+          pass
+    
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''def scrap():
+    config = tw.Config()
+    config.Limit = 500 #Total tweets to scrape
+    config.Store_CSV = True
+    config.Output = "menciones.csv"
+    config.Search = 'Banco Popular'
+    tw.run.Search(config)
+
+
+
+if __name__ == "__main__":
+   scrap()'''
