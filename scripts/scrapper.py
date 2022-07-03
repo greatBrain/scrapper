f345e56ba858fa5a84a795efb3fdec7f8b22e1cb
greatbrain <erickfania@gmail.com>
2022-07-03 10:30:45 -0400

    Searching method and results written in a csv file

--- scripts/scrapper.py
+++ scripts/scrapper.py
@@ -1,30 +1,43 @@
 import twint as tw
-
+from datetime import datetime
+from datetime import date
+import os
 
 class Scrapper:
-      def __init__(self):
-          self.keywords = list()
+      def __init__(self):          
 
+          self.KEYWORDS = [ 
+          'Banco Popular', 
+          'AFP Popular', 
+          'Grupo Popular', 
+          'AFI Popular', 
+          'Inversiones Popular',          
+          ]          
       
+      def _get_key(self):
+          for key in self.KEYWORDS:
+              yield key              
+
       def search(self):
+          all_keywords = list(self._get_key())
+
+          for keyw in range(len(all_keywords)):
+              config = tw.Config()
+
+              #Total tweets to scrape
+              config.Limit = 300           
+              config.Store_CSV = True
+              config.Output = os.path.join("../reports/menciones_{}_.csv".format(datetime.today().strftime('%d-%m-%Y')))
+              config.Search = all_keywords[keyw]
+              tw.run.Search(config)
+          
+      def filter_data(self):
           pass
-    
-      
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+   scrp = Scrapper()
+   scrp.search()
 
 
 
